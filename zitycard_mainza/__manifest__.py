c26adb6181a3e9c3914f41d62cd9c6253726d1e1
acapdevilazitycard <acapdevila@zitycard.com>
2025-01-30 08:59:07 +0100

    [IPM] Nuevo campo en presupuestos

--- zitycard_mainza/__manifest__.py
+++ zitycard_mainza/__manifest__.py
@@ -12,12 +12,14 @@
         'account',
         'stock',
         'website_sale_stock',
-        'purchase'
+        'purchase',
+        'sale',
     ],
     'data': [
         'reports/report_deliveryslip.xml',
         'reports/report_stockpicking_operations.xml',
         'views/product_template_views.xml',
+        'views/sale_order_views.xml',
         'views/portal/product_template_portal_views.xml',
     ],
 	'icon': '/zitycard_mainza/static/description/icon.png',
--- zitycard_mainza/models/__init__.py
+++ zitycard_mainza/models/__init__.py
@@ -3,3 +3,4 @@ from . import product_product
 from . import product_supplier_info
 from . import product_template
 from . import purchase_order
+from . import sale_order
