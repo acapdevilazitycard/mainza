3489c382740754e87b2c9ee4a80eb2db294307f9
acapdevilazitycard <acapdevila@zitycard.com>
2025-02-13 08:11:23 +0100

    [IMP] Ajustes en reports de compra/venta y template portal

--- zitycard_mainza/__manifest__.py
+++ zitycard_mainza/__manifest__.py
@@ -20,6 +20,7 @@
         'reports/report_stockpicking_operations.xml',
         'reports/sale_order_reports.xml',
         'reports/account_move_reports.xml',
+        'reports/purchase_order_templates.xml',
         'views/product_template_views.xml',
         'views/sale_order_views.xml',
         'views/portal/product_template_portal_views.xml',
